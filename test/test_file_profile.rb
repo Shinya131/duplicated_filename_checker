67f24dd4f7faca46255397eb49244b2c20860097
Shinya131 <nagai3mt5b@gmail.com>
2015-03-06 13:07:10 +0900

    Refactoring: change val name

--- test/test_file_profile.rb
+++ test/test_file_profile.rb
@@ -2,14 +2,18 @@ require 'minitest_helper'
 
 class TestFileProfile < MiniTest::Unit::TestCase
   def setup
-    @file_profile = DuplicatedFilenameChecker::FileProfile.new('./test/sample_for_test/dir_a1/duplicate_filename.sample')
+    @sample_filename      = 'duplicate_filename.sample'
+    @sample_relative_path = File.join('./test/sample_for_test/dir_a1/', @sample_filename)
+    @sample_fullpath      = File.join(test_fullpath, 'sample_for_test/dir_a1/', @sample_filename)
+
+    @file_profile = DuplicatedFilenameChecker::FileProfile.new(@sample_relative_path)
   end
 
   def test_basename
-    assert @file_profile.basename == 'duplicate_filename.sample'
+    assert @file_profile.basename == @sample_filename
   end
 
   def test_path
-    assert @file_profile.path == File.join(test_fullpath, 'sample_for_test/dir_a1/duplicate_filename.sample')
+    assert @file_profile.path == @sample_fullpath
   end
 end

cd0fce802dc85d60004564bf59f7e428b1c54598
Shinya131 <nagai3mt5b@gmail.com>
2015-03-06 12:46:42 +0900

    Add: FileProfile#basename

--- lib/duplicated_filename_checker/file_profile.rb
+++ lib/duplicated_filename_checker/file_profile.rb
@@ -1,2 +1,9 @@
 class DuplicatedFilenameChecker::FileProfile
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def basename
+    File.basename @file_path
+  end
 end
--- test/test_file_profile.rb
+++ test/test_file_profile.rb
@@ -2,10 +2,10 @@ require 'minitest_helper'
 
 class TestFileProfile < MiniTest::Unit::TestCase
   def setup
-    @file_profile = DuplicatedFilenameChecker::FileProfile.new
+    @file_profile = DuplicatedFilenameChecker::FileProfile.new('./sample_for_test/dir_a1/duplicate_filename.sample')
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_basename
+    assert @file_profile.basename == 'duplicate_filename.sample'
   end
 end

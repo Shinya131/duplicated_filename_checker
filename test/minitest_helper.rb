ecf971906451d9eafd59732194a81844182ccec0
Shinya131 <nagai3mt5b@gmail.com>
2015-03-06 19:56:49 +0900

    Change coveralls setting

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,11 +2,6 @@
 require 'coveralls'
 Coveralls.wear!
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-]
-SimpleCov.start
-
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'duplicated_filename_checker'
 

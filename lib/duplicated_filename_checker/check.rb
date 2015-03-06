1c249ef8ecf2b2993f44954648b1fc5ede151772
Shinya131 <nagai3mt5b@gmail.com>
2015-03-06 14:40:58 +0900

    Refactoring

--- lib/duplicated_filename_checker/check.rb
+++ lib/duplicated_filename_checker/check.rb
@@ -1,6 +1,6 @@
 class DuplicatedFilenameChecker::Check
   def initialize(*survey_direcotry_roots)
-    set_survey_target_paths_by!(survey_direcotry_roots)
+    @survey_target_paths =  survey_target_paths_by(survey_direcotry_roots)
   end
 
   def execute
@@ -9,6 +9,7 @@ class DuplicatedFilenameChecker::Check
 
   private
 
+  # select duplicate paths from survey target paths
   def duplicate_paths
     @survey_target_paths.select do |path|
       is_duplicate?(path.basename)
@@ -19,24 +20,17 @@ class DuplicatedFilenameChecker::Check
     survey_paths_basename_list.count(basename) > 1
   end
 
-  # baseames of survey_paths. it list is not uniq.
+  # Baseames of survey_paths. It list is not uniq.
   # @example ['basename1', 'basename1', 'basename2', 'basename3']
   def survey_paths_basename_list
     @basename_list ||= @survey_target_paths.map(&:basename) #memorize
   end
 
-  def set_survey_target_paths_by!(root_paths)
-    paths = survey_paths_by(root_paths)
-    @survey_target_paths = paths.map{|path| to_file_profile(path) }
-  end
+  def survey_target_paths_by(root_paths)
+    # get paths in subdirectory
+    paths = root_paths.map{|root| Dir.glob("#{root}/**/*.*") }.flatten
 
-  def survey_paths_by(survey_direcotry_roots)
-    survey_direcotry_roots.map do |root|
-      Dir.glob("#{root}/**/*.*")
-    end.flatten
-  end
-
-  def to_file_profile(path)
-    DuplicatedFilenameChecker::FileProfile.new(path)
+    # exchange to FileProfile
+    paths.map{|path| DuplicatedFilenameChecker::FileProfile.new(path) }
   end
 end

a0278fa1953c801387609c712fa8eec931186110
Shinya131 <nagai3mt5b@gmail.com>
2015-03-06 13:54:05 +0900

    Fake it

--- lib/duplicated_filename_checker/check.rb
+++ lib/duplicated_filename_checker/check.rb
@@ -2,5 +2,32 @@ require 'digest/md5'
 
 class DuplicatedFilenameChecker::Check
   def initialize(*survey_direcotry_paths)
+    @survey_direcotry_paths = survey_direcotry_paths.map{|path| File.absolute_path path }
+  end
+
+  def execute
+    @file_paths = []
+    @survey_direcotry_paths.each do |root|
+      unless File.exist?(root)
+        raise "No such file or directory: #{root}"
+      end
+
+      Dir.glob("#{root}/**/*.*").each do |path|
+        @file_paths << DuplicatedFilenameChecker::FileProfile.new(path)
+      end
+    end
+
+    # 重複を探す
+    duplicate_file_paths = []
+
+    @file_paths.each do |path|
+      @file_base_names ||= @file_paths.map(&:basename) # メモ化
+
+      if @file_base_names.count(path.basename) > 1 # duplicate
+        duplicate_file_paths << path
+      end
+    end
+
+    duplicate_file_paths.group_by(&:basename)
   end
 end
--- test/test_check.rb
+++ test/test_check.rb
@@ -2,13 +2,27 @@ require 'minitest_helper'
 
 class TestCheck < MiniTest::Unit::TestCase
   def setup
-    survey_dir_path_1 = './sample_for_test/dir_a1'
-    survey_dir_path_2 = './sample_for_test/dir_b1'
+    survey_dir_path_1 = './test/sample_for_test/dir_a1'
+    survey_dir_path_2 = './test/sample_for_test/dir_b1'
+    @sample_filename  = 'duplicate_filename.sample'
 
     @check = DuplicatedFilenameChecker::Check.new(survey_dir_path_1, survey_dir_path_2)
   end
 
-  def test_initialize
-    assert true
+  def test_execute
+    duplicate_file_paths = @check.execute
+
+    assert(duplicate_file_paths.class == Hash)
+
+    # key is duplicate filename
+    basename = duplicate_file_paths.keys.first
+    assert(basename == @sample_filename)
+
+    # value is duplicate file paths
+    filepaths = duplicate_file_paths.values.first
+
+    assert filepaths.all?{ |path| path.class == DuplicatedFilenameChecker::FileProfile }
+    assert filepaths.all?{ |path| path.basename == @sample_filename } # all basename is same
+    assert filepaths.map(&:path) == filepaths.map(&:path).uniq        # all path is different
   end
 end

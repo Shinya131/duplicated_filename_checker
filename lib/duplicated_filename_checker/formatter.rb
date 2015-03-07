36f12e06af01fa4dac5e771e792de57ca38185d5
Shinya131 <nagai3mt5b@gmail.com>
2015-03-07 17:25:50 +0900

    Add Formatter#exclude_basename

--- lib/duplicated_filename_checker/formatter.rb
+++ lib/duplicated_filename_checker/formatter.rb
@@ -3,7 +3,23 @@ class DuplicatedFilenameChecker::Formatter
     @duplicate_file_profiles = result_of_duplicate_filename_checker_check_execute
   end
 
+  def exclude_basename(exclude_basename_regexp)
+    @exclude_basename_regexp = exclude_basename_regexp
+  end
+
   def only_basename
-    @duplicate_file_profiles.keys
+    duplicate_file_profiles_with_filter.keys
+  end
+
+  private
+
+  def duplicate_file_profiles_with_filter
+    unless @exclude_basename_regexp
+      return @duplicate_file_profiles
+    end
+
+    @duplicate_file_profiles.select do |basename, _|
+      !basename =~ Regexp.new(@exclude_basename_regexp)
+    end
   end
 end
